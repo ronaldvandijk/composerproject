2819b0cdddd251115a2be37685001db14b080114
Ronald van Dijk <r.van.dijk@pl.hanze.nl>
2017-01-23 13:42:27 +0100

    modified AbstractDuckFactory to play a little with GIT

--- composerproject/src/composerproject/AbstractDuckFactory.java
+++ composerproject/src/composerproject/AbstractDuckFactory.java
@@ -2,6 +2,7 @@ package composerproject;
 
 public abstract class AbstractDuckFactory {
 	abstract public MallardDuck createMallardDuck();
+	/*blaaaa */
 	abstract public RedheadDuck createRedheadDuck();
 	abstract public RubberDuck createRubberDuck();
 }
